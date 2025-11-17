e935dd1fef18e2e0b68b4a343a6d7ddeadf0ba91
Amirhossein Mohammadi <amirhossein1384m@gmail.com>
2025-11-16 22:39:12 -0500

    Capture UserID during registration

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -8,12 +8,12 @@ const generateToken = (id) => {
 
 // Register user
 export const registerUser = async (req, res) => {
-  const { name, email, password } = req.body;
+  const { UserID, name, email, password } = req.body;
   try {
     const userExists = await User.findOne({ email });
     if (userExists) return res.status(400).json({ message: "User already exists" });
 
-    const user = await User.create({ name, email, password });
+    const user = await User.create({ UserID, name, email, password });
     res.status(201).json({
       _id: user._id,
       name: user.name,
