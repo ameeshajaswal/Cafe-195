38b4ea85c9f570b8dab7320e4bc6b24b224a5073
Serey-Vaddh-Savy <ssavy@mycentennialcollege.ca>
2025-11-18 22:05:58 -0500

    Updated 60% of the landingPage

--- client/src/landingPage.jsx
+++ client/src/landingPage.jsx
@@ -1,67 +1,37 @@
 import React, { useState } from "react";
 
 function LandingPage(){
-    const [iceLatte, setIceLatte] = useState(0);
-    const [croissant, setCroissant] = useState(0);
-    const iceLattePrice = 4.25;
-    const croissantPrice = 5;
-
-    function addCroissant(){
-        setCroissant(croissant + 1);
-    }
-
-    function removeCroissant(){
-        if(croissant > 0){
-            setCroissant(croissant - 1);
-        }
-    }
-
-    function addIceLatte(){
-        setIceLatte(iceLatte + 1);
-    }
-
-    function removeIceLatte(){
-        if(iceLatte > 0){
-            setIceLatte(iceLatte - 1);
-        }
-    }
-
-    function submitOrder(){
-
-        if(croissant == 0 && iceLatte == 0){
-            window.alert('Please select an item before ordering');
-        }
-        else{
-            window.alert("Order submitted");
-            window.location.reload();
-        }
-    }
-
+    
     return(
         <header>
-            <div>
-                <h1>Welcome to Cafe 195</h1>
-                <img src="../public/logo.png" alt="" style={{width: 100}}/>
+            <nav>
+                <a href="">HOME</a>
+                <a href="">DRINKS</a>
+                <a href="">FOODS</a>
+                <a href="">ABOUT</a>
+                <a href="">CONTACT</a>
+            </nav>
 
-                <label style={{display: 'block'}} htmlFor="">Ice latte</label>
-                <button onClick={addIceLatte}>Add</button>
-                <button onClick={removeIceLatte}>Remove</button>
+            <div id="userInformationSectionLandingPage">
+                    <a href="">CART</a>
+                    <a href="">SIGN UP</a>
+                    <a href="">LOGIN</a>
 
-                <p>Drinks: Ice Latte - {iceLatte} - ${(iceLatte * iceLattePrice).toFixed(2)}</p>
+                </div>
 
-
-                <label style={{display: 'block'}} htmlFor="">Croissant</label>
-                <button onClick={addCroissant}>Add</button>
-                <button onClick={removeCroissant}>Remove</button>
-
-                <p>Food: Croissant - {croissant} - ${(croissant * croissantPrice).toFixed(2)}</p>
-
-
-
+            <div id="landPageMainDisplay">
+                <img src="../public/coffee.png" alt="" id="landingPageCoffeePicBack" />
+                <img src="../public/coffee pic 2.png" alt=""  id="landingPageCoffeePicFront"/>
                 
 
-                <button onClick={submitOrder}>Order Now</button>
+                <h1>195°F</h1>
+                <div id="designLineLandingPage"></div>
+                <p>Perfect Shot</p>
+
+                <button id="startOrderingBtn">Start Ordering</button>
+
             </div>
+            
         </header>
     )
 }
