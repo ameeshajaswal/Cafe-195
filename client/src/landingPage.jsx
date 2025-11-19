e8299068f383ce80a895ffc3190bb4dc1d4a7f91
Serey-Vaddh-Savy <ssavy@mycentennialcollege.ca>
2025-11-18 23:15:45 -0500

    Finished landingPage design, not yet the function

--- client/src/landingPage.jsx
+++ client/src/landingPage.jsx
@@ -4,6 +4,9 @@ function LandingPage(){
     
     return(
         <header>
+            <video autoPlay muted loop id="backgroundVideo">
+                <source src="../public/landingPageVideo.mp4" type="video/mp4" />
+            </video>
             <nav>
                 <a href="">HOME</a>
                 <a href="">DRINKS</a>
@@ -11,25 +14,18 @@ function LandingPage(){
                 <a href="">ABOUT</a>
                 <a href="">CONTACT</a>
             </nav>
-
             <div id="userInformationSectionLandingPage">
                     <a href="">CART</a>
                     <a href="">SIGN UP</a>
                     <a href="">LOGIN</a>
-
                 </div>
-
             <div id="landPageMainDisplay">
-                <img src="../public/coffee.png" alt="" id="landingPageCoffeePicBack" />
-                <img src="../public/coffee pic 2.png" alt=""  id="landingPageCoffeePicFront"/>
-                
-
-                <h1>195°F</h1>
-                <div id="designLineLandingPage"></div>
-                <p>Perfect Shot</p>
-
-                <button id="startOrderingBtn">Start Ordering</button>
-
+                <div id="introductionContainer">
+                    <h1>195°F</h1>
+                    <div id="designLineLandingPage"></div>
+                    <p>Perfect Shot</p>
+                    <button id="startOrderingBtn">Start Ordering</button>
+                </div>
             </div>
             
         </header>
